b491aa6581c8a198c5d1c05933ede3f5469e1a67
Kelvin Njuguna <159237362+njugus@users.noreply.github.com>
2024-07-12 03:19:09 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,14 +3,6 @@
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
   import { GoogleAuthProvider , getAuth, signInWithPopup, FacebookAuthProvider  } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
  
-  const firebaseConfig = {
-    apiKey: "AIzaSyCw16Ftj7nXmbIWKZW-Peqpvuh-tM9tvuk",
-    authDomain: "login-project-efcb8.firebaseapp.com",
-    projectId: "login-project-efcb8",
-    storageBucket: "login-project-efcb8.appspot.com",
-    messagingSenderId: "435716421978",
-    appId: "1:435716421978:web:82d06a972e60b7b1a75b63"
-  };
 
   const app = initializeApp(firebaseConfig);
   const provider = new GoogleAuthProvider();
